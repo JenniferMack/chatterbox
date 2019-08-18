f58bc78d386e19f543fe010abf18bf514a78bce6
Jennifer <JenniferMack@users.noreply.github.com>
2019-08-18 07:30:12 -0700

    build out app

--- cmd/chatterbox/main.go
+++ cmd/chatterbox/main.go
@@ -4,11 +4,23 @@ import (
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/JenniferMack/chatterbox"
 )
 
 func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
+
+	chat, err := chatterbox.NewServer("5050")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer chat.Close()
+
+	room := chatterbox.NewRoom("Lobby")
+	defer room.Close()
+	go room.Accept(chat)
 
 	<-quit
 	println()
