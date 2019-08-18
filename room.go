1a1adb7d817e3ab9dd0820fc52597fb1c67cf8e7
Jennifer <JenniferMack@users.noreply.github.com>
2019-08-18 07:46:38 -0700

    add room listener

--- room.go
+++ room.go
@@ -17,18 +17,27 @@ type chatroom struct {
 func (cr *chatroom) Connect(c net.Conn) {
 	client := &client{
 		write: bufio.NewWriter(c),
+		read:  bufio.NewReader(c),
 		recv:  cr.messages,
+		send:  make(chan []byte),
 	}
 
 	cr.mu.Lock()
 	cr.clients[c] = client
 	cr.mu.Unlock()
 	go client.relay()
+	go client.monitor()
 }
 
 func (cr *chatroom) Send(msg []byte) {
 	for _, client := range cr.clients {
 		client.recv <- msg
+	}
+}
+
+func (cr *chatroom) listen() {
+	for byt := range cr.messages {
+		cr.Send(byt)
 	}
 }
 
